ad1be7600013927586073481d810755adf158274
obadea <obadeagbenga@gmail.com>
2024-01-22 19:15:57 +0100

    Added new feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -44,7 +44,7 @@ const NavBar = () => {
   };
 
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light relative">
       <button
         className=" flex-col justify-center items-center p-1.5 hidden lg:flex"
         onClick={handleClick}
@@ -65,7 +65,8 @@ const NavBar = () => {
           }`}
         ></span>
       </button>
-      <div className="w-full flex justify-center items-center lg:hidden">
+
+      <div className="w-full flex justify-between items-center lg:hidden">
         <nav>
           <CustomLink href="/" title="Home" className="mr-4" />
           <CustomLink href="/about" title="About" className="m-4" />
@@ -133,6 +134,76 @@ const NavBar = () => {
           </button>
         </nav>
       </div>
+
+      <div className="min-w-[70vw] flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2  z-30 -translate-y-1/2 bg-dark/90 dark:bg-light/60 rounded-lg  py-32  backdrop-blur-md ">
+        <nav className="flex items-center justify-center flex-col">
+          <CustomLink href="/" title="Home" className="mr-4" />
+          <CustomLink href="/about" title="About" className="m-4" />
+          <CustomLink href="/projects" title="Projects" className="mx-4" />
+          <CustomLink href="/articles" title="Articles" className="ml-4" />
+        </nav>
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            href="https://twitter.com/obadea0"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3 dark:fill-light"
+          >
+            <TwitterIcon />
+          </motion.a>
+          <motion.a
+            href="https://github.com/obadea"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <GithubIcon />
+          </motion.a>
+          <motion.a
+            href="www.linkedin.com/in/obadea"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <LinkedInIcon />
+          </motion.a>
+          <motion.a
+            href="/"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3 dark:bg-light rounded-full"
+          >
+            <PinterestIcon />
+          </motion.a>
+          <motion.a
+            href="/"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 ml-3   "
+          >
+            <DribbbleIcon />
+          </motion.a>
+
+          <button
+            onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}
+            className={`ml-3 flex items-center justify-center rounded-full p-1
+          ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'}
+          `}
+          >
+            {mode === 'dark' ? (
+              <SunIcon className={'fill-dark'} />
+            ) : (
+              <MoonIcon className={'fill-dark'} />
+            )}
+          </button>
+        </nav>
+      </div>
+
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
       </div>
