24c1b32e31ee97e5c1222ebd7784e9756631a5f5
obadea <obadeagbenga@gmail.com>
2024-01-29 20:15:17 +0100

    Updated Resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/GbengaObadiahResume.pdf"
+                  href="/Obadiah Gbenga Resume.pdf"
                   target="_blank"
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
                   
