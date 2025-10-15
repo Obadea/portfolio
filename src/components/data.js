0024da2d9311b044c7ad52d65c6679b0831e3060
obadea <obadeagbenga@gmail.com>
2025-10-15 12:22:29 +0100

    worked on new projects

--- src/components/data.js
+++ src/components/data.js
@@ -1,142 +1,151 @@
-import project1 from '../../public/images/projects/carHub.jpeg';
-import project2 from '../../public/images/projects/Clipboard.jpeg';
-import project3 from '../../public/images/projects/fylo.jpeg';
-import project4 from '../../public/images/projects/gpt-3.jpeg';
-import project5 from '../../public/images/projects/promptopia.jpeg';
-import project6 from '../../public/images/projects/portfolio.jpeg';
-import project7 from '../../public/images/projects/forkify.jpeg';
-import project8 from '../../public/images/projects/metaVerse.jpeg';
-import project9 from '../../public/images/projects/bankist website.jpeg';
-import project10 from '../../public/images/projects/mapty.jpeg';
-import project11 from '../../public/images/projects/aiSummarizer.jpeg';
-import project12 from '../../public/images/projects/movieland.jpeg';
-import project13 from '../../public/images/projects/obadeaTrackPreview.png';
-import project14 from '../../public/images/projects/obadinn.jpeg';
-import project15 from '../../public/images/projects/elegant-collection.jpeg';
-import project16 from '../../public/images/projects/nike.jpeg';
-import project17 from '../../public/images/projects/iphone.jpeg';
-import project18 from '../../public/images/projects/alphaProject.jpeg';
+import project1 from "../../public/images/projects/carHub.jpeg";
+import project2 from "../../public/images/projects/Clipboard.jpeg";
+import project3 from "../../public/images/projects/fylo.jpeg";
+import project4 from "../../public/images/projects/gpt-3.jpeg";
+import project5 from "../../public/images/projects/promptopia.jpeg";
+import project6 from "../../public/images/projects/portfolio.jpeg";
+import project7 from "../../public/images/projects/forkify.jpeg";
+import project8 from "../../public/images/projects/metaVerse.jpeg";
+import project9 from "../../public/images/projects/bankist website.jpeg";
+import project10 from "../../public/images/projects/mapty.jpeg";
+import project11 from "../../public/images/projects/aiSummarizer.jpeg";
+import project12 from "../../public/images/projects/movieland.jpeg";
+import project13 from "../../public/images/projects/obadeaTrackPreview.png";
+import project14 from "../../public/images/projects/obadinn.jpeg";
+import project15 from "../../public/images/projects/elegant-collection.jpeg";
+import project16 from "../../public/images/projects/nike.jpeg";
+import project17 from "../../public/images/projects/iphone.jpeg";
+import project18 from "../../public/images/projects/alphaProject.jpeg";
+import project19 from "../../public/images/projects/resumind.jpeg";
 
 export const items = [
   {
-    title: 'Car ShowCase Application',
-    category: 'Latest',
-    link: 'https://obadea-cars-showcase.vercel.app/',
-    github: 'https://github.com/Obadea/obadea_cars_showcase/',
-    type: 'Nextjs | IMAGIN.studio API',
+    title: "Car ShowCase Application",
+    category: "Latest",
+    link: "https://obadea-cars-showcase.vercel.app/",
+    github: "https://github.com/Obadea/obadea_cars_showcase/",
+    type: "Nextjs | IMAGIN.studio API",
     img: project1,
   },
   {
-    title: 'GPT-3',
-    category: 'Latest',
-    link: 'https://gpt-3-rv4.pages.dev/',
-    github: 'https://github.com/Obadea/GPT-3',
-    type: 'UI/UX Focus',
+    title: "GPT-3",
+    category: "Latest",
+    link: "https://gpt-3-rv4.pages.dev/",
+    github: "https://github.com/Obadea/GPT-3",
+    type: "UI/UX Focus",
     img: project4,
   },
   {
-    title: 'Alpha Dashboard',
-    category: 'React Latest',
-    link: 'https://alphatwelve-project.vercel.app/',
-    github: 'https://github.com/Obadea/Alphatwelve_ProjectNike',
-    type: 'Responsive Design | Tailwind css',
+    title: "RESUMIND",
+    category: "Latest",
+    link: "https://ai-resume-summarizer-eight.vercel.app/",
+    github: "https://github.com/Obadea/ai-resume-summarizer",
+    type: "UI/UX Focus || AI",
+    img: project19,
+  },
+  {
+    title: "Alpha Dashboard",
+    category: "React Latest",
+    link: "https://alphatwelve-project.vercel.app/",
+    github: "https://github.com/Obadea/Alphatwelve_ProjectNike",
+    type: "Responsive Design | Tailwind css",
     img: project18,
   },
   {
-    title: 'Forkify',
-    category: 'JavaScript',
-    link: 'https://forkify-test.pages.dev/',
-    github: 'https://github.com/Obadea/Forkify',
-    type: 'Responsive Design | API Integration',
+    title: "Forkify",
+    category: "JavaScript",
+    link: "https://forkify-test.pages.dev/",
+    github: "https://github.com/Obadea/Forkify",
+    type: "Responsive Design | API Integration",
     img: project7,
   },
   {
-    title: 'Bankist Website',
-    category: 'JavaScript',
-    link: 'https://bankist-webpage.pages.dev',
-    github: 'https://github.com/Obadea/bankist-webpage',
-    type: 'Responsive Design | Interactivity',
-    category: 'Nextjs',
+    title: "Bankist Website",
+    category: "JavaScript",
+    link: "https://bankist-webpage.pages.dev",
+    github: "https://github.com/Obadea/bankist-webpage",
+    type: "Responsive Design | Interactivity",
+    category: "Nextjs",
     img: project9,
   },
   {
-    title: 'MetaVerse',
-    category: 'Latest',
-    link: 'https://meta-verse-obadea.vercel.app/',
-    github: 'https://github.com/Obadea/metaVerse',
-    type: 'Responsive Design | Animation and Interactivity',
+    title: "MetaVerse",
+    category: "Latest",
+    link: "https://meta-verse-obadea.vercel.app/",
+    github: "https://github.com/Obadea/metaVerse",
+    type: "Responsive Design | Animation and Interactivity",
     img: project8,
   },
   {
-    title: 'Fylo',
-    category: 'UI/UX Focus',
-    link: 'https://obadea.github.io/fylo/',
-    github: 'https://github.com/Obadea/fylo',
-    type: 'Responsive Design',
+    title: "Fylo",
+    category: "UI/UX Focus",
+    link: "https://obadea.github.io/fylo/",
+    github: "https://github.com/Obadea/fylo",
+    type: "Responsive Design",
     img: project3,
   },
 
   {
-    title: 'Promptopia',
-    category: 'NextJs',
-    link: 'https://promptopia-obadea.vercel.app/',
-    github: 'https://github.com/Obadea/promptopia',
-    type: 'API Integration',
+    title: "Promptopia",
+    category: "NextJs",
+    link: "https://promptopia-obadea.vercel.app/",
+    github: "https://github.com/Obadea/promptopia",
+    type: "API Integration",
     img: project5,
   },
   {
     title: "Obadiah's Portfolio",
-    link: '/',
-    github: 'https://github.com/Obadea/portfolio',
-    type: 'Animation and Interactivity | Performance Optimization',
+    link: "/",
+    github: "https://github.com/Obadea/portfolio",
+    type: "Animation and Interactivity | Performance Optimization",
     img: project6,
   },
   {
-    title: 'Mapty',
-    category: 'JavaScript',
-    link: 'https://obadea.github.io/Mapty/',
-    github: 'https://github.com/Obadea/Mapty',
-    type: 'API Integration',
+    title: "Mapty",
+    category: "JavaScript",
+    link: "https://obadea.github.io/Mapty/",
+    github: "https://github.com/Obadea/Mapty",
+    type: "API Integration",
     img: project10,
   },
   {
-    title: 'Ai Summarizer',
-    category: 'React',
-    link: 'https://obadea-ai-summarizer.pages.dev/',
-    github: 'https://github.com/Obadea/Ai-summarizer',
-    type: 'API Integration',
+    title: "Ai Summarizer",
+    category: "React",
+    link: "https://obadea-ai-summarizer.pages.dev/",
+    github: "https://github.com/Obadea/Ai-summarizer",
+    type: "API Integration",
     img: project11,
   },
   {
-    title: 'Obadea Track',
-    category: 'TypeScript',
-    link: 'https://obadeatrack.pages.dev/',
-    github: 'https://github.com/Obadea/obadeaTrack',
-    type: 'Refined(CRUD)',
+    title: "Obadea Track",
+    category: "TypeScript",
+    link: "https://obadeatrack.pages.dev/",
+    github: "https://github.com/Obadea/obadeaTrack",
+    type: "Refined(CRUD)",
     img: project13,
   },
   {
-    title: 'Elegant Collection',
-    category: 'Full Stack',
-    link: 'https://elegant-collection-ecommerce.pages.dev',
-    github: '/projects',
-    type: 'E-Commerce',
+    title: "Elegant Collection",
+    category: "Full Stack",
+    link: "https://elegant-collection-ecommerce.pages.dev",
+    github: "/projects",
+    type: "E-Commerce",
     img: project15,
   },
   {
-    title: '3D Iphone Website',
-    category: 'React',
-    link: 'https://3d-iphone-website.pages.dev/',
-    github: 'https://github.com/Obadea/3d-Iphone-website',
-    type: 'GSAP Animation, Three.js and Interactivity | Performance Optimization',
+    title: "3D Iphone Website",
+    category: "React",
+    link: "https://3d-iphone-website-ten.vercel.app/",
+    github: "https://github.com/Obadea/3d-Iphone-website",
+    type: "GSAP Animation, Three.js and Interactivity | Performance Optimization",
     img: project17,
   },
   {
-    title: 'Nike',
-    category: 'React',
-    link: 'https://nike-b4d.pages.dev/',
-    github: 'https://github.com/Obadea/Nike',
-    type: 'Responsive Design | Tailwind css',
+    title: "Nike",
+    category: "React",
+    link: "https://nike-b4d.pages.dev/",
+    github: "https://github.com/Obadea/Nike",
+    type: "Responsive Design | Tailwind css",
     img: project16,
   },
 ];
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,73 +1,16 @@
-import AnimatedText from '@/components/AnimatedText';
-import { GithubIcon } from '@/components/Icons';
-import Layout from '@/components/Layout';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import AnimatedText from "@/components/AnimatedText";
+import { GithubIcon } from "@/components/Icons";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-import { motion } from 'framer-motion';
-import { items } from '../components/data';
-import TransitionEffect from '@/components/TransitionEffect';
-import Slider from '@/components/Slider';
+import { motion } from "framer-motion";
+import { items } from "../components/data";
+import TransitionEffect from "@/components/TransitionEffect";
+import Slider from "@/components/Slider";
 const FramerImage = motion(Image);
-
-// const FeaturedProject = ({ type, title, summary, img, link, github }) => {
-//   return (
-//     <article className="w-full flex items-center rounded-br-2xl justify-between rounded-3xl bg-light shadow-2xl p-12 dark:bg-dark dark:shadow-3xl lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
-//       <Link
-//         href={link}
-//         target="_blank"
-//         className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
-//       >
-//         <FramerImage
-//           src={img}
-//           alt={title}
-//           className="w-full h-auto "
-//           whileHover={{ scale: 1.05 }}
-//           transition={{ duration: 0.2 }}
-//           priority
-//           sizes="(max-width:768px) 100vw,
-//           (max-width: 1200px) 50vw,
-//           50vw"
-//         />
-//       </Link>
-
-//       <div className="w-1/2 flex flex-col items-start justify-between pl-6  lg:w-full lg:pl-0 lg:pt-6">
-//         <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">
-//           {type}
-//         </span>
-//         <Link
-//           href={link}
-//           target="_blank"
-//           className="hover:underline underline-offset-2"
-//         >
-//           <h2 className="my-2 w-full text-left text-3xl font-bold dark:text-light sm:text-sm">
-//             {title}
-//           </h2>
-//         </Link>
-//         <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">
-//           {summary}
-//         </p>
-//         <div className="mt-2 flex items-center">
-//           <Link href={github} target="_blank" className="w-10">
-//             <GithubIcon />
-//           </Link>
-
-//           <Link
-//             href={link}
-//             target="_blank"
-//             className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base
-
-//             "
-//           >
-//             Visit Project
-//           </Link>
-//         </div>
-//       </div>
-//     </article>
-//   );
-// };
 
 const Project = ({ title, type, img, link, github }) => {
   return (
@@ -122,13 +65,13 @@ const Projects = () => {
   const [filteredItems, setFilteredItems] = useState(items);
 
   let filters = [
-    'Latest',
-    'UI/UX Focus',
-    'React',
-    'JavaScript',
-    'Nextjs',
-    'TypeScript',
-    'Full Stack',
+    "Latest",
+    "UI/UX Focus",
+    "React",
+    "JavaScript",
+    "Nextjs",
+    "TypeScript",
+    "Full Stack",
   ];
 
   const handleFilterButtonClick = (selectedCategory) => {
@@ -177,8 +120,8 @@ const Projects = () => {
                 onClick={() => handleFilterButtonClick(category)}
                 className={`filter__button ${
                   selectedFilters?.includes(category)
-                    ? 'bg-dark text-light dark:bg-light dark:text-dark '
-                    : ''
+                    ? "bg-dark text-light dark:bg-light dark:text-dark "
+                    : ""
                 }`}
                 key={`filters-${idx}`}
               >
